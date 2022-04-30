c914539d6edf4efd723cfd21c861cb1f105843f3
Aldo pereira <aldopereira.gyn@gmail.com>
2022-04-30 14:44:27 -0300

    AppoimentFactory configurando.

--- factories/AppointmentFactory.js
+++ factories/AppointmentFactory.js
@@ -2,16 +2,20 @@ class AppointmentFactory {
 
     Build(simpleAppointment) {
         // id, title, start, end
+        // console.log("AppointmentFactory: 5", simpleAppointment)
 
         let day = simpleAppointment.date.getDate() + 1
         let month = simpleAppointment.date.getMonth()
         let year = simpleAppointment.date.getFullYear()
 
-        let hour = Number.parseInt(simpleAppointment.time.split(':')[0])
+        let hours = Number.parseInt(simpleAppointment.time.split(':')[0])
         let minutes = Number.parseInt(simpleAppointment.time.split(':')[1])
 
-        let startDate = new Date(year, month, day, hour, minutes, 0, 0)
+        // let startDate = new Date(year, month, day, hour, minutes, 0, 0)
+
+        let startDate = new Date(year, month, day, hours, minutes, 0, 0)
         startDate.setHours(startDate.getHours() - 3)
+
 
         let appo = {
             id: simpleAppointment._id,
